4188943a56e6fad0a5a4cf7551f9568d060db2e2
Pierre-Olivier Bouteau <pierre-olivier.bouteau@broadpeak.tv>
2022-12-13 14:49:58 +0100

    add license

--- app/src/main/java/tv/broadpeak/smartlib/demo/android/PlayerActivity.java
+++ app/src/main/java/tv/broadpeak/smartlib/demo/android/PlayerActivity.java
@@ -5,22 +5,30 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.PlaybackException;
 import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.source.LoadEventInfo;
+import com.google.android.exoplayer2.source.MediaLoadData;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.MediaSourceEventListener;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.ui.StyledPlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import tv.broadpeak.smartlib.SmartLib;
+import tv.broadpeak.smartlib.ad.AdManager;
+import tv.broadpeak.smartlib.engine.manager.LoggerManager;
 import tv.broadpeak.smartlib.session.streaming.StreamingSession;
 import tv.broadpeak.smartlib.session.streaming.StreamingSessionResult;
 
@@ -50,6 +58,62 @@ public class PlayerActivity extends AppCompatActivity {
         // Create SmartLib session
         mSession = SmartLib.getInstance().createStreamingSession();
 
+        // Activate advertising
+        mSession.activateAdvertising();
+
+        // Ad events
+        mSession.setAdEventsListener(new AdManager.AdEventsListener() {
+            /**
+             * Triggered when ad break begin
+             *
+             * @param position Ad break begin position in millis
+             * @param duration Ad break duration in millis
+             */
+            @Override
+            public void onAdBreakBegin(long position, long duration) {
+                // Lock player controls
+            }
+
+            /**
+             * Triggered when an ad begin
+             *
+             * @param position Ad begin position in millis
+             * @param duration Ad duration in millis
+             * @param clickURL Ad click URL, empty string if unset
+             */
+            @Override
+            public void onAdBegin(long position, long duration, String clickURL) {
+                // Show ad link button if needed
+            }
+
+            /**
+             * Triggered when an ad is skippable
+             *
+             * @param offset position in the complete content where skip become allowed (in milliseconds from complete playlist)
+             * @param limit position in milliseconds where seek need to be done
+             */
+            @Override
+            public void onAdSkippable(long offset, long limit) {
+                // Show the skip message/button "skip ad in x seconds"
+            }
+
+            /**
+             * Triggered when the ad is ended, not called if skipped
+             */
+            @Override
+            public void onAdEnd() {
+                // Unlock player controls, hide ad link, hide ad skip button
+            }
+
+            /**
+             * Triggered when ad break ended, even in case of skipping
+             */
+            @Override
+            public void onAdBreakEnd() {
+                // Unlock player controls, hide ad link, hide ad skip button
+            }
+        });
+
         // Attach the player on the same thread
         mSession.attachPlayer(mPlayer);
 
@@ -64,12 +128,31 @@ public class PlayerActivity extends AppCompatActivity {
                     // Create a data source factory.
                     DataSource.Factory dataSourceFactory = new DefaultHttpDataSource.Factory();
 
-                    // Create a HLS media source pointing SmartLib result URL
-                    HlsMediaSource hlsMediaSource = new HlsMediaSource.Factory(dataSourceFactory)
-                            .createMediaSource(MediaItem.fromUri(result.getURL()));
+                    // Create a media source pointing SmartLib result URL
+                    MediaSource mediaSource = null;
+                    if (result.getURL().contains(".mpd")) {
+                        mediaSource = new DashMediaSource.Factory(dataSourceFactory)
+                                .createMediaSource(MediaItem.fromUri(result.getURL()));
+                    } else if (result.getURL().contains(".m3u8")) {
+                        mediaSource = new HlsMediaSource.Factory(dataSourceFactory)
+                                .createMediaSource(MediaItem.fromUri(result.getURL()));
+                    }
 
                     // Attach media source events to the SmartLib session
-                    hlsMediaSource.addEventListener(new Handler(Looper.getMainLooper()), (MediaSourceEventListener) mSession.getListener());
+                    mediaSource.addEventListener(new Handler(Looper.getMainLooper()), (MediaSourceEventListener) mSession.getListener());
+                    mediaSource.addEventListener(new Handler(), new MediaSourceEventListener() {
+                        @Override
+                        public void onLoadCanceled(int windowIndex, @Nullable MediaSource.MediaPeriodId mediaPeriodId, LoadEventInfo loadEventInfo, MediaLoadData mediaLoadData) {
+                            LoggerManager.getInstance().printErrorLogs("ExoPlayer", "onLoadCanceled");
+                        }
+
+                        @Override
+                        public void onLoadError(int windowIndex, @Nullable MediaSource.MediaPeriodId mediaPeriodId, LoadEventInfo loadEventInfo, MediaLoadData mediaLoadData, IOException error, boolean wasCanceled) {
+                            LoggerManager.getInstance().printErrorLogs("ExoPlayer", "onLoadError wasCanceled:" + wasCanceled);
+                            LoggerManager.getInstance().printErrorLogs("ExoPlayer", "onLoadError uri:" + loadEventInfo.uri);
+                            error.printStackTrace();
+                        }
+                    });
 
                     // On non-recoverable error, stop the current session
                     mPlayer.addListener(new Player.Listener() {
@@ -80,7 +163,7 @@ public class PlayerActivity extends AppCompatActivity {
                     });
 
                     // Set the media item to be played.
-                    mPlayer.setMediaSource(hlsMediaSource);
+                    mPlayer.setMediaSource(mediaSource);
 
                     // Prepare the player.
                     mPlayer.prepare();
