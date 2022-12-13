50f8d4e252a20e87865297ef4caa465aebc78905
Pierre-Olivier Bouteau <pierre-olivier.bouteau@broadpeak.tv>
2022-12-13 09:47:00 +0100

    initial commit

---
+++ app/src/main/java/tv/broadpeak/smartlib/demo/android/MainActivity.java
@@ -0,0 +1,14 @@
+package tv.broadpeak.smartlib.demo.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
